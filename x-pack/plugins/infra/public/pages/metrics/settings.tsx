43839df46cd615615c76973bfd2d7e7809d75612
Jenny <dzheni.pavlova@elastic.co>
2022-09-08 19:12:53 +0200

    Add settings page documnent title

--- x-pack/plugins/infra/public/pages/metrics/settings.tsx
+++ x-pack/plugins/infra/public/pages/metrics/settings.tsx
@@ -8,12 +8,24 @@
 import { EuiErrorBoundary } from '@elastic/eui';
 import React from 'react';
 import { useKibana } from '@kbn/kibana-react-plugin/public';
+import { i18n } from '@kbn/i18n';
 import { SourceConfigurationSettings } from './settings/source_configuration_settings';
+import { DocumentTitle } from '../../components/document_title';
 
 export const MetricsSettingsPage = () => {
   const uiCapabilities = useKibana().services.application?.capabilities;
   return (
     <EuiErrorBoundary>
+      <DocumentTitle
+        title={(previousTitle: string) =>
+          i18n.translate('xpack.infra.infrastructureSettingsPage.documentTitle', {
+            defaultMessage: '{previousTitle} | Settings',
+            values: {
+              previousTitle,
+            },
+          })
+        }
+      />
       <SourceConfigurationSettings
         shouldAllowEdit={uiCapabilities?.infrastructure?.configureSource as boolean}
       />

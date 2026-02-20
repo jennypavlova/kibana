33e02e11009ee6d79e647704becc2f6df29c1958
Jenny <dzheni.pavlova@elastic.co>
2026-02-20 11:53:58 +0100

    [APM] Migrate AWS Lambda cypress tests to scout to fix the flakiness

--- x-pack/solutions/observability/plugins/apm/test/scout/ui/fixtures/page_objects/service_details/overview_tab.ts
+++ x-pack/solutions/observability/plugins/apm/test/scout/ui/fixtures/page_objects/service_details/overview_tab.ts
@@ -22,6 +22,8 @@ export class OverviewTab extends ServiceDetailsTab {
   public readonly instancesLatencyDistribution: Locator;
   public readonly serviceOverviewInstancesTable: Locator;
 
+  public readonly coldstartRateChart: Locator;
+
   constructor(page: ScoutPage, kbnUrl: KibanaUrl, defaultServiceName: string) {
     super(page, kbnUrl, defaultServiceName);
     this.tab = this.page.getByTestId(`${this.tabName}Tab`);
@@ -31,6 +33,7 @@ export class OverviewTab extends ServiceDetailsTab {
     this.serviceOverviewErrorsTable = this.page.getByTestId('serviceOverviewErrorsTable');
     this.instancesLatencyDistribution = this.page.getByTestId('instancesLatencyDistribution');
     this.serviceOverviewInstancesTable = this.page.getByTestId('serviceOverviewInstancesTable');
+    this.coldstartRateChart = this.page.getByTestId('coldstartRate');
   }
 
   protected async waitForTabLoad(): Promise<void> {
